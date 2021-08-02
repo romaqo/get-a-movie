3a0b93b88883b47902f879ddddda9b85573aeac3
romaqo <roman.korsun7@gmail.com>
2021-08-02 14:59:55 +0300

    Basic infrastructure

---
+++ src/main/java/com/romaqo/getamovie/GetAMovieApplication.java
@@ -0,0 +1,13 @@
+package com.romaqo.getamovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetAMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetAMovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/romaqo/getamovie/GetAMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romaqo.getamovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetAMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
